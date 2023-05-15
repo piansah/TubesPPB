e728f88ed1eaa136efa994f3d62511566966d780
Pian <alifalpian157@gmail.com>
2023-05-15 21:45:20 +0700

    code

--- app/src/main/java/com/example/sharedpreferences/User.kt
+++ app/src/main/java/com/example/sharedpreferences/User.kt
@@ -1,3 +1,3 @@
 package com.example.sharedpreferences
 
-data class User(val id : Int = -1, val email : String, val password : String)
+data class User(val id : Int = -1, val email : String, val password : String, val firstName : String, val lastName :String)
--- app/src/main/java/com/example/sharedpreferences/UserContract.kt
+++ app/src/main/java/com/example/sharedpreferences/UserContract.kt
@@ -7,6 +7,8 @@ object UserContract {
         const val TABLE_NAME = "user"
         const val COLUMN_ID = "id"
         const val COLUMN_EMAIL = "email"
+        const val COLUMN_FIRSTNAME = "firstName"
+        const val COLUMN_LASTNAME = "lastName"
         const val COLUMN_PASSWORD = "password"
     }
 }
